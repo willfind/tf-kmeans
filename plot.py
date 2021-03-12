16a55ac0ab5a5e60e1611a89713b82b70d8a2b30
jrc03c <jrc03c@pm.me>
2021-03-12 12:48:48 -0600

    ...

---
+++ plot.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plot
+
+def count(item, arr):
+    return len(list(filter(lambda v: v == item, arr)))
+
+data = pd.read_csv("results.csv")
+errors = data["learnedKs"] - data["actualKs"]
+x = list(sorted(set(errors)))
+y = list(count(e, errors) for e in x)
+plot.bar(x, y)
+plot.show()
