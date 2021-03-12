cae61a121a593514e3884aeee5c1e61864b30f80
jrc03c <jrc03c@pm.me>
2021-03-12 12:49:45 -0600

    ...

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plot
 
 def count(item, arr):
-    return len(list(filter(lambda v: v == item, arr)))
+  return len(list(filter(lambda v: v == item, arr)))
 
 data = pd.read_csv("results.csv")
 errors = data["learnedKs"] - data["actualKs"]
