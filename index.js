aabd924a7df59fc5389671b46a1f953d07dd5180
jrc03c <jrc03c@pm.me>
2021-03-13 12:47:14 -0600

    ...

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("js-math-tools").dump()
-const KMeansCV = require("./k-means-cv.js")
-// const plotly = require("plotly.js-dist")
+const KMeans = require("./k-means.js")
+const plotly = require("plotly.js-dist")
 
 const subtract = (a, b) => add(a, scale(b, -1))
 const divide = (a, b) => scale(a, pow(b, -1))
@@ -11,50 +11,58 @@ function normalize(x){
 }
 
 // weird cases: 11255 w/ completely random; 10145 with 7 centroids in a circle
+let theSeed = round(random() * 10000) + 10000
+seed(theSeed)
+let k = round(random() * 6) + 3
+let centroids = normal([k, 2])
 
-let rScores = []
-let actualKs = []
-let learnedKs = []
-let seeds = []
+let x = []
 
-for (let iteration=0; iteration<100; iteration++){
-  let theSeed = round(random() * 10000) + 10000
-  seed(theSeed)
-  let k = round(random() * 6) + 3
-  let centroids = normal([k, 2])
-
-  let x = []
-
-  for (let i=0; i<100; i++){
-    let c = centroids[parseInt(random() * centroids.length)]
-    x.push(add(c, scale(0.1, normal(2))))
-  }
-
-  x = new DataFrame(x)
-  normalize(x)
-
-  let kValues = range(1, 16)
-
-  let kmeans = new KMeansCV({
-    kValues,
-    maxIterations: 15,
-    maxRestarts: 25,
-    numberOfFolds: 4,
-    shouldShuffle: false,
-  })
-
-  let scores = kmeans.fit(x)
-
-  console.log("===============================")
-  console.log("learned k:", kmeans.centroids.length)
-  console.log("actual k:", k)
-  console.log("seed:", theSeed)
-  console.log("iteration:", iteration)
-
-  seeds.push(theSeed)
-  learnedKs.push(kmeans.centroids.length)
-  actualKs.push(k)
+for (let i=0; i<100; i++){
+  let c = centroids[parseInt(random() * centroids.length)]
+  x.push(add(c, scale(0.1, normal(2))))
 }
 
-let out = new DataFrame({seeds, learnedKs, actualKs})
-out.toCSV("results.csv")
+x = new DataFrame(x)
+normalize(x)
+
+let kmeans = new KMeans({k})
+kmeans.fit(x, progress => console.log(progress.toFixed(2)))
+
+// plot
+function createContainer(width, height){
+  let container = document.createElement("div")
+  container.style.width = `${width}px`
+  container.style.height = `${height}px`
+  document.body.appendChild(container)
+  return container
+}
+
+const width = 512
+const height = 512
+
+plotly.newPlot(createContainer(width, height), [
+  {
+    name: "centroids",
+    x: kmeans.centroids.map(c => c[0]),
+    y: kmeans.centroids.map(c => c[1]),
+    mode: "markers",
+    type: "scatter",
+    marker: {
+      color: "red",
+      size: 15,
+    },
+  },
+
+  {
+    name: "data",
+    x: x.values.map(p => p[0]),
+    y: x.values.map(p => p[1]),
+    mode: "markers",
+    type: "scatter",
+    marker: {
+      color: "black",
+      size: 4,
+    },
+  },
+])
