bebee3fab17251facdf96668ba6edb068fd5a8f0
jrc03c <jrc03c@pm.me>
2021-03-20 18:53:58 -0500

    ...

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-require("js-math-tools").dump()
-const { subtract, divide, isMatrix } = require("./helpers.js")
+require("./all.js")
 const KMeans = require("./k-means.js")
+// const plotly = require("plotly.js-dist")
+
+function createContainer(width, height){
+  let out = document.createElement("div")
+  out.style.width = `${width}px`
+  out.style.height = `${height}px`
+  document.body.appendChild(out)
+  return out
+}
 
 function normalize(x){
   assert(isMatrix(x), "`x` should be a matrix!")
@@ -17,8 +25,8 @@ function normalize(x){
 
 seed(12345)
 
-let rows = 250
-let cols = 15
+let rows = 100
+let cols = 2
 let k = round(random() * 6) + 3
 let centroids = normal([k, cols])
 
@@ -37,4 +45,39 @@ let kmeans = new KMeans({
   maxRestarts: 25,
 })
 
-kmeans.fit(x).then(() => console.log("Done!"))
+kmeans.fit(x).then(async () => {
+  console.log("Done!")
+
+  // // plot
+  // let centroids = await kmeans.centroids.array()
+  //
+  // plotly.newPlot(createContainer(512, 512), [
+  //   {
+  //     name: "centroids",
+  //     x: centroids.map(c => c[0]),
+  //     y: centroids.map(c => c[1]),
+  //     mode: "markers",
+  //     type: "scatter",
+  //     marker: {
+  //       size: 15,
+  //       color: "red",
+  //     },
+  //   },
+  //
+  //   {
+  //     name: "data",
+  //     x: x.map(v => v[0]),
+  //     y: x.map(v => v[1]),
+  //     mode: "markers",
+  //     type: "scatter",
+  //     marker: {
+  //       size: 4,
+  //       color: "black",
+  //     },
+  //   },
+  // ])
+}).catch(e => {
+  console.error(e)
+  kmeans.destroy()
+  console.log("Destroyed!")
+})
