4eff72918aa36c291731a972949c7521268b4eab
jrc03c <jrc03c@pm.me>
2021-03-25 11:37:01 -0500

    Removed a bunch of extra files.

--- all.js
+++
@@ -1,7 +0,0 @@
-require("js-math-tools").dump()
-global.tf = require("@tensorflow/tfjs")
-let helpers = require("./helpers.js")
-
-Object.keys(helpers).forEach(key => {
-  global[key] = helpers[key]
-})
