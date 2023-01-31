3ecf97c0f1be32b630710aa9b953a485028be44b
jrc03c <jrc03c@pm.me>
2023-01-31 09:24:24 -0600

    ...

--- src/models/tf-kmeans-meta.js
+++ src/models/tf-kmeans-meta.js
@@ -1,5 +1,13 @@
-const { assert, isFunction, isUndefined } = require("@jrc03c/js-math-tools")
+const {
+  assert,
+  isFunction,
+  isEqual,
+  isUndefined,
+  shape,
+} = require("@jrc03c/js-math-tools")
+
 const { KMeansMeta } = require("@jrc03c/js-data-science-helpers").KMeans
+const { isTFTensor } = require("../helpers")
 const TFKMeansPlusPlus = require("./tf-kmeans-plus-plus")
 
 class TFKMeansMeta extends KMeansMeta {
@@ -17,6 +25,49 @@ class TFKMeansMeta extends KMeansMeta {
 
     const self = this
     self.modelClass = config.modelClass || TFKMeansPlusPlus
+  }
+
+  get centroids() {
+    const self = this
+
+    if (self.fittedModel) {
+      return self.fittedModel.centroids
+    } else {
+      return self._fitState.bestCentroids
+    }
+  }
+
+  set centroids(centroids) {
+    const self = this
+
+    if (isTFTensor(centroids)) {
+      centroids = centroids.arraySync()
+    }
+
+    const newShape = shape(centroids)
+
+    if (self.centroids) {
+      const oldCentroids = self.centroids.arraySync()
+      const oldShape = shape(oldCentroids)
+
+      assert(
+        isEqual(newShape, oldShape),
+        `The new centroids must have the same shape as the old centroids! (expected: [${oldShape.join(
+          ", "
+        )}], received: [${newShape.join(", ")}])`
+      )
+    }
+
+    if (!self.fittedModel) {
+      self.fittedModel = new self.modelClass({
+        k: newShape[0],
+        maxIterations: self.maxIterations,
+        maxRestarts: self.maxRestarts,
+        tolerance: self.tolerance,
+      })
+    }
+
+    self.fittedModel.centroids = centroids
   }
 }
 
